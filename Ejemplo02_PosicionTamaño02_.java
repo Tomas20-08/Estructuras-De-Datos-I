99ea4ccc68a92b9629039681b79b8cbcc598ac8c
Tomás David León Montoya <tdleon@ucompensar.edu.co>
2026-02-05 18:07:39 -0500

    Array

---
+++ Ejemplo02_PosicionTamaño02_.java
@@ -0,0 +1,22 @@
+
+package Array;
+
+public class Ejemplo02_PosicionTamaño02_ {
+
+    public static void main(String[] args) {
+        
+        int[]num = {13,1,25};
+
+        int x = 0;
+        
+        for (int i = 0; i < 3; i++) {
+            
+            x++;
+            
+            System.out.println("posicion " + i + " num "+ x + " ... " + num[i]);
+            
+        }
+
+    }
+    
+}
