c3301fc685b805be4f99582e4098e31485bd6055
Tomás David León Montoya <tdleon@ucompensar.edu.co>
2026-02-05 18:09:43 -0500

    Quiz

---
+++ Ejercicio01_PreguntasCerradas_.java
@@ -0,0 +1,99 @@
+//Preguntas Cerradas Quiz 1 
+
+package Quiz;
+
+public class Ejercicio01_PreguntasCerradas_ {
+
+    public static void main(String[] args) {
+        
+                                                                              /*
+        
+1. ¿Cuál es el nombre que recibe un programa java ?
+                            
+JavaDoc
+
+JavaCode
+
+ByteCode  ✔ 
+
+BitCode
+        
+
+2. La estructura que observas en la imagen, ¿es correcta?
+
+switch (<operación>)
+
+{
+
+case <expresión>:
+
+...
+break;
+
+case <expresión>:
+
+...
+break;
+
+default;
+...
+
+}
+
+
+Falso
+
+Verdadero  ✔ 
+
+
+        
+3. Es una estructura condicional la cual emplea casos para poder continuar con su código:
+
+While
+
+If - Else
+
+If
+
+Switch  ✔ 
+        
+        
+        
+4. Son los ciclos en los que se define la cantidad de iteraciones. 
+
+While
+
+Do - while
+
+For  ✔ 
+
+Switch
+        
+        
+5. Distingue entre mayúsculas y minúsculas
+
+Camel case
+
+Case sensitive ✔ 
+
+Case mayus
+
+Camel sensitive
+        
+        
+6. Nombre dado a una variable, clase, o método:
+
+Estatica
+
+Int
+
+Ámbito
+
+Identificador ✔ 
+        
+
+                                                                              */
+
+    }
+    
+}
---
+++ Ejercicio02_PreguntasAbiertas_.java
@@ -0,0 +1,74 @@
+//Preguntas Abiertas Quiz 1 
+
+package Quiz;
+
+public class Ejercicio02_PreguntasAbiertas_ {
+
+    public static void main(String[] args) {
+        
+                                                                              /*
+        
+1. ¿Qué imprime el código que ves en la imágen? 
+
+package Quiz;
+
+public class Ejercicio01_PreguntasCerradas_ {
+
+    public static void main(String[] args) {
+
+for(i = 0; i<10; i++){
+
+        System.out.println(" " + i);
+
+                     }
+        
+    }
+        
+}
+        
+Respuesta: Nada/Error, Debido a que nose se declara el tipo de variable que es i
+        
+        
+2. Escriba la estructura básica (en código) que tiene una condición if - else. 
+        
+        int a = 10;
+        int b = 5; 
+        
+if (a > b){ 
+           System.out.println("true"); 
+} if else { 
+           System.out.println("false"); 
+} else {
+           System.out.println("No se cumple la condición"); }      
+        
+        Salida: true ✔
+        
+3. Escribe las 3 líneas básicas que debe tener un programa en Java para escribir un programa.
+        
+        package MiClase; 
+        
+        public class MiClase { 
+        
+        public static void main(string[]args) { 
+        
+        }
+        
+    }
+        
+4. Escribe un ejemplo sencillo de un contador utilizando el ciclo do While. Este contador debe ir de 9 a 0.
+        
+        int i = 9;
+        
+        do {
+ 
+        System.out.println("Num... " + i); 
+        i-- ;
+        
+        }while(i>=0);
+        
+                                                                              */ 
+
+
+    }
+    
+}
